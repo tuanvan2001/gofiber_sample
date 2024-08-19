20228be6c59c6af8c4e052699f9bdc9fc9a7eb50
Tuấn Văn <tuandev2001@gmail.com>
2024-08-19 14:57:27 +0700

    update

--- Controllers/User.go
+++ Controllers/User.go
@@ -40,7 +40,7 @@ func (uc *UserController) CreateUser(ctx *fiber.Ctx) error {
 			"errors":  validationErr.Error(),
 		})
 	}
-	var existingUserByEmail, existingUserByUsername, existingUserByPhone any
+	var existingUserByEmail, existingUserByUsername, existingUserByPhone interface{}
 	Configs.MySQL.Where("email = ?", body.Email).First(&existingUserByEmail)
 
 	Configs.MySQL.Where("username = ?", body.Username).First(&existingUserByUsername)
