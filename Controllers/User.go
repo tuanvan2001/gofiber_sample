e2f8f66f815c26012eae3ca64bab2c6dd1543e19
Tuấn Văn <tuandev2001@gmail.com>
2024-08-20 10:33:33 +0700

    uppercase file

--- Controllers/User.go
+++ Controllers/User.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func CreateUser(ctx *fiber.Ctx) error {
+func CreateUser(ctx *fiber.Ctx) any {
 	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
 		"message": "success",
 		"data": fiber.Map{
